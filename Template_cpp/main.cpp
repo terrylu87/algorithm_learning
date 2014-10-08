e115776fbb0fba47a46977329f158ceea1a7293b
terrylu <terrylu87@gmail.com>
2014-10-08 15:40:03 +0800

    add template program

---
+++ Template_cpp/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2012/04/18 16:56:38
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  TerryLu (), terrylu87@gmail.com
+ *        Company:  TerryLu's working house
+ *
+ * =====================================================================================
+ */
+
+/* test data */
+
+#include <iostream>
+//#include <stdlib.h>
+
+using namespace std;
+
+int main(int argc,char ** argv)
+{
+    cout << "hello haker" << endl;
+
+    return 0;
+}
