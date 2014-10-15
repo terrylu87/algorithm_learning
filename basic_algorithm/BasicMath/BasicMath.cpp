35758f5508c6c7a4d1f48241b9335eddc95d6185
terrylu87 <terrylu87@gmail.com>
2014-10-15 16:12:25 +0800

    add functions in BasicMath.h

--- basic_algorithm/BasicMath/BasicMath.cpp
+++ basic_algorithm/BasicMath/BasicMath.cpp
@@ -35,3 +35,59 @@ bool isPrime (int n)
 
    return true;
 }
+
+//assume that a and b cannot both be 0
+int GCD(int a, int b)
+{
+   if (b==0) return a;
+   return GCD(b,a%b);
+}
+
+int LCM(int a, int b)
+{
+   return b*a/GCD(a,b);
+}
+
+int toDecimal(int n, int b)
+{
+   int result=0;
+   int multiplier=1;
+
+   while(n>0)
+   {
+      result+=n%10*multiplier;
+      multiplier*=b;
+      n/=10;
+   }
+
+   return result;
+}
+
+int fromDecimal(int n, int b)
+{
+   int result=0;
+   int multiplier=1;
+
+   while(n>0)
+   {
+      result+=n%b*multiplier;
+      multiplier*=10;
+      n/=b;
+   }
+
+   return result;
+}
+
+string fromDecimal2(int n, int b)
+{
+   string chars="0123456789ABCDEFGHIJ";
+   string result="";
+
+   while(n>0)
+   {
+      result=chars.at(n%b) + result;
+      n/=b;
+   }
+
+   return result;
+}
--- basic_algorithm/BasicMath/BasicMath.h
+++ basic_algorithm/BasicMath/BasicMath.h
@@ -20,6 +20,18 @@
 #ifndef _BASICMATH_H_
 #define _BASICMATH_H_
 
+#include <string>
+
 bool isPrime (int n);
+
+int GCD(int a, int b);
+
+int LCM(int a, int b);
+
+int toDecimal(int n, int b);
+
+int fromDecimal(int n, int b);
+
+std::string fromDecimal2(int n, int b);
 
 #endif /* _BASICMATH_H_ */
